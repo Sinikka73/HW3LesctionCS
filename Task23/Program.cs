de56c1cf479d098bf59da919df349de9cc420068
olga <bend_sinister@mail.ru>
2022-09-27 10:23:17 +0300

    T23

---
+++ Task23/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    
+    {
+        Console.Write($"Введите целое число больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+int number = getNumberFromUser("Введите целое число N > 1: ");
+
+
+int[] tableQuarterNumber(int number)
+{
+    int[] result = new int [number];
+    for(int i = 1; i <= number; i++)
+    {
+        result[i - 1] = (int)(Math.Pow(i, 3));
+    }
+    return result;
+}
+
+int[] table = tableQuarterNumber(number);
+
+
+for (int i = 0; i < table.Length; i++)
+{
+    Console.Write(table[i] + " ");
+} 
