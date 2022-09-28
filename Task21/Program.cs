e788f9e4e0973512a23fc9655a50d9dfa1ebe601
olga <bend_sinister@mail.ru>
2022-09-28 10:05:41 +0300

    T21 текстовое оформление

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -7,20 +7,20 @@ int getCoordinateFromUser(string userInformation)
     Console.Write($"{userInformation} ");
     while (!int.TryParse(Console.ReadLine(), out result))
     {
-        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+        Console.Write($"Введите целое число. {userInformation} ");
     }
     return result;
 }
-
 double findDistanceTwoPoints3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2)
 {
     return Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
 }
 
-int userX1 = getCoordinateFromUser("X1 = ");
-int userY1 = getCoordinateFromUser("Y1 = ");
-int userZ1 = getCoordinateFromUser("Z1 = ");
-int userX2 = getCoordinateFromUser("X2 = ");
-int userY2 = getCoordinateFromUser("Y2 = ");
-int userZ2 = getCoordinateFromUser("Z3 = ");
-Console.WriteLine(findDistanceTwoPoints3D(userX1, userY1, userZ1, userX2, userY2, userZ2));
+int userX1 = getCoordinateFromUser("Введите координату X1 = ");
+int userY1 = getCoordinateFromUser("Введите координату Y1 = ");
+int userZ1 = getCoordinateFromUser("Введите координату Z1 = ");
+int userX2 = getCoordinateFromUser("Введите координату X2 = ");
+int userY2 = getCoordinateFromUser("Введите координату Y2 = ");
+int userZ2 = getCoordinateFromUser("Введите координату Z3 = ");
+Console.Write("Расстояние между точками: ");
+Console.Write(findDistanceTwoPoints3D(userX1, userY1, userZ1, userX2, userY2, userZ2));
